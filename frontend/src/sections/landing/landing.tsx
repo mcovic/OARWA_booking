d3b0df72f904609bafa817d465df646480de4210
mcovic <marincovic4@gmail.com>
2025-07-11 23:58:25 +0200

    add responsive values to typography

--- frontend/src/sections/landing/landing.tsx
+++ frontend/src/sections/landing/landing.tsx
@@ -1,7 +1,23 @@
+import { Box, Stack, Typography } from '@mui/material';
+
+// ----------------------------------------------------------------------
+
 export default function Landing() {
 
     return (
-        'Landing Section Placeholder'
+        <Box
+            sx={{
+                py: 10,
+                display: 'grid',
+                placeContent: 'center',
+            }}
+        >
+            <Stack spacing={2}>
+                <Typography variant={'h1'} fontWeight={500}>
+                    Dobrodošli!
+                </Typography>
+            </Stack>
+        </Box>
     );
 
 }
