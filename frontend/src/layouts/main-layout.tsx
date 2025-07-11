e0210fc7090b5bde356acfc7448cf976639fbe00
mcovic <marincovic4@gmail.com>
2025-07-11 22:45:29 +0200

    add navbar, add logo

--- frontend/src/layouts/main-layout.tsx
+++ frontend/src/layouts/main-layout.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from '@layouts/index.ts';
 import { Box } from '@mui/material';
 import { Outlet } from 'react-router';
 
@@ -12,8 +13,12 @@ export default function MainLayout() {
                 height: 1,
                 display: 'flex',
                 flexDirection: 'column',
+                px: { xs: 2, sm: 3, lg: 4 },
+                py: 2,
             }}
         >
+            <Navbar />
+
             <Box
                 sx={{
                     flexGrow: 1,
--- frontend/src/pages/landing/landing-page.tsx
+++ frontend/src/pages/landing/landing-page.tsx
@@ -1,4 +1,5 @@
 import { Box } from '@mui/material';
+import Landing from '@sections/landing/landing.tsx';
 
 // ----------------------------------------------------------------------
 
@@ -11,6 +12,7 @@ export default function LandingPage(){
             <Box sx={{
                 height: 1,
             }}>
+                <Landing />
             </Box>
         </>
     );
